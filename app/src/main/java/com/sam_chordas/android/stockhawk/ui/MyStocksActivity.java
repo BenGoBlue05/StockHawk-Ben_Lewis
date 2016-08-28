9110832082f24b66dcbe556f64ae210b6dfcd41b
BenGoBlue05 <bplewis5@gmail.com>
2016-08-28 10:54:59 -0500

    fix hard coded strings

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
@@ -132,7 +132,7 @@ public class MyStocksActivity extends AppCompatActivity
                                     if (c != null) {
                                         if (c.getCount() != 0) {
                                             Toast toast =
-                                                    Toast.makeText(MyStocksActivity.this, "This stock is already saved!",
+                                                    Toast.makeText(MyStocksActivity.this, getString(R.string.stock_already_saved),
                                                             Toast.LENGTH_LONG);
                                             toast.setGravity(Gravity.CENTER, Gravity.CENTER, 0);
                                             toast.show();
